7ed94ead39f1e36d43cac1797a7cd5b4bb958280
BobAfwata <bobraphtoneafwata@gmail.com>
2025-01-29 23:57:17 +0900

    data types in python

---
+++ 1_data_types.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+
+# linux , unix operating system , python interpeter ...installed path /usr/bin/env python
+
+# Data types
+
+#snake case 
+
+# declare the variable 
+maths_grade = "A"
+physics_grade = 'C'
+
+#Camel case 
+biologyGrade = "D"
+
+print("===================================")
+print("My Maths grade is : ",maths_grade) # use  the variable  
+print("Physics Grade is  : ",physics_grade)
+print("Biology Grade is  : ",biologyGrade)
+print("===================================")
+
+print("     *    ")
+print("*    *     *")
+print("*    *           *")
+print("*    *     *    *")
+print("******")
+print("*")
+print("*")
+print("*")
+
+#triangle 
+#pyramid patterns 
+# X mass tree
+
+#string --> collection of characters 
+name = "Lesley" 
+grade_1 = "A #$$        "
+grade_2 = "B12"
+
+grade_3 = grade_1 + grade_2
+
+print(grade_3)
+print(name)
+
+#intergers - > positive / neg whole numbers 
+
+age = 24 
+time_hrs = 5
+distance = 240 
+
+print("I am  {} years old ".format(age))
+
+print("The time taken from Nairobi to Kisumu is {0} hrs , a distance of {1} kms ".format(time_hrs,distance))
+print("The time taken from Nairobi to Kisumu is {1} hrs , a distance of {0} kms ".format(time_hrs,distance))
+
+# floating points -> fractions / decimal point numbers 
+height = 167.2  # cm
+weight = 67.89
+
+bmi = weight / ( height * height)
+print("Your Weight is  : {0} kgs and height is :  {1} cm and a bmi of : {2}".format(weight,height,bmi))
+
+#boolean - True / false 
+
+is_female = False
+is_male = True
+is_done = True
+
+
+#assignment 
+
+#calculate speed of a vehicle : distance = 200 , time = 3
+# speed = distance / time 
+# print 
+
+
+# area of a circle 
+
+
+
+# volume and surface are of a cylinder 
+# v = pi * radius * radius * h 
+
+pi = 3.142
+radius = 70 
+height = 100 
+
+v = pi * radius * radius * height
+
+print(v)
+
+
+# calculate Pressure applied to a surface when a force of 120N acts on a square surface of 200 cm
+
+
