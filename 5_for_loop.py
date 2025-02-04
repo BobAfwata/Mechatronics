8bd167d57cae5597a210a69ffb73f4e0ac85f9ef
BobAfwata <bobraphtoneafwata@gmail.com>
2025-02-05 01:07:32 +0900

    if else

---
+++ 5_for_loop.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python 
+
+# loops and iterations 
+
+# for loop
+# while loop 
+# if statements
+# if else  statements 
+# switch case 
+
+#for x in range(0 n) ==> n-1
+
+
+# space is called a tab and should not be deleted 
+
+for x in range(1,21,2):
+    print(x)
+
+for i in range(0,100,10):
+    print(i)
+    
---
+++ sine_cosine.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python 
+
+# write a program using math libary to
+#  print the sine ,cosine ,tangeng of angles from - 180 -> + 180  
+import math 
+
+angle = 90
+
+
+
+for angle in range(-180,180,30):
+    print(math.sin(angle))
+    print(math.cos(angle))
+    print(math.tan(angle))
+
