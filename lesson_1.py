e558d7c6c4f66c347696f6181dd9f67cd606bc3e
BobAfwata <bobraphtoneafwata@gmail.com>
2025-02-11 00:16:37 +0900

    loops ,strings, integers ,format output

---
+++ lesson_1.py
@@ -0,0 +1,14 @@
+print("My name is Bob Afwata ")
+print("I am from Kenya ")
+print("I like watching movies ")
+
+# format output
+
+first_name = "Bob"
+second_name = "Afwata"
+age = 26
+weight = 58.9
+
+# fstring 
+
+print(f"My name is  {first_name} {second_name},I am {age} years old and weigh {weight} kgs ")
---
+++ loops.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import math
+
+
+for x in range(0,6,2): # statement 
+    print(x)
+
+#even numbers between 0 to 100
+
+for numbers in range(1,10,2):
+    print(numbers)
+
+print("I study in Korea")
+
+x = 40 # statement
+math.cos(x)
+
+#wap to print odd numbers from 0 to 30
+#     wap to print the factorial of a number given as input 
+#     number = input("enter the number")
+#     fact_number = number * (number - 1) ---> in a loop
+#wap to claculate the volume of a cylinder v = pi * r *r * h
+
