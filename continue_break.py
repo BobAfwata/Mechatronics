1e35321a60721b6508e3116eca1f735f863f930e
BobAfwata <bobraphtoneafwata@gmail.com>
2025-02-13 21:36:11 +0900

    while loop

---
+++ continue_break.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+# break : 
+
+number  = 0
+while number < 10 :
+    print(number)
+    if number == 4 :
+        print("Number 4 is reached ")
+        break 
+    number = number + 1
+# continue
---
+++ while_loop.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python 
+
+# prints numbers in a loop
+
+"""
+
+number = 0 # initial condition 1 ....9  10
+while  number < 10:   # loop
+    number = number + 1  #pre increment 1 
+    print(number)   # 1 2 ....9  10
+    #number = number + 1  #post increment  1 2
+
+"""   
+# wap that prints numbers from 10 to 0 (descending order )
+
+count = 10 # initial condition 
+while count > 0 :
+    count = count - 1 # pre decrement
+    print(count)
+
+#factorial --> 5! = 5 * 4 * 3 *2 * 1  
+#              n! = n * (n-1) * (n-2) * (n-3) .........*(n-m) * n*m * 1  n > 0
+
+
