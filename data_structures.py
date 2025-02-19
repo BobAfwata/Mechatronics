6dfb3bad5eed62ed4827ee378103ef203cb0633f
BobAfwata <bobraphtoneafwata@gmail.com>
2025-02-19 22:27:27 +0900

    Arrays and Lists

---
+++ data_structures.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python 
+from array import *
+
+# arrays 
+# arrays start at 0
+
+ages = array('i' ,[23,45,67,78,89,29]) # i - integers
+
+"""
+print(ages[0])
+print(ages[1])
+print(ages[5])
+print(ages[-1])
+"""
+
+
+print(ages)
+ages.append(73)
+ages.append(63)
+ages.append(71)
+print(ages)
+
+ages[2] = 75
+
+ages.pop()
+print(ages)
+
+ages.reverse()
+print(ages)
+
+#print(ages[6]) # out of index error
+
+#print(len(ages)) # 6 items
+# create an array of 5 numbers
+
+
+
+
+#list
+our_ages = [23,56,67,89,12,67]
---
+++ lists.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+# list of fruits 
+#mutable - size / content
+fruits = ["Lemon", "Orange","Apple","Kiwi","Peaches","Mangoes","Guavas"] #string
+
+print(fruits) 
+fruits[0] = "Banana"
+print(fruits) 
+
+fruits[-3] = "Pineapple"
+print(fruits) 
+
+# for loop to print all fruits 
+
+for fruit in fruits:
+    print(fruit)
+
+fruits.append("Water Mellon")
+fruits.append("Lime")
+fruits.append("Avocado")
+
+print(fruits)
+fruits.pop()
+print(fruits)
+fruits.pop()
+print(fruits)
+
+
+fruits.insert(3,"Pawpaw")
+
+fruits.extend(["Tomatoes","Cherry","Grapes"])
+print(fruits)
+
+#fruits.sort()
+#fruits.clear()
+#print(fruits)
