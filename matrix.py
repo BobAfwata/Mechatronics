fa83d6b290a522e2868068d31c13db74e70b9b7c
BobAfwata <bobraphtoneafwata@gmail.com>
2025-02-25 22:09:29 +0900

    data structures

--- matrix.py
+++ matrix.py
@@ -5,11 +5,29 @@
 # A * B = 
 # A inverse  
 # Dot Product ,Cross Product
-
+# multidimensional arrays 
+# R x C
 
 # list of a list 
 numbers = [1,2,3]
-matrix_A = [[1,2,3],[4,5,6],[7,8,9]]
+
+for number in numbers:
+    print(number)
+
+print("------------------------------------------")
+
+matrix_A = [[1,2,3],[4,5,6],[7,8,9],[10,11,12]]
+
+
+for i in range(4):
+    for j in range(3):
+        print(matrix_A[i][j])
+
+print("------------------------------------------")
+print(matrix_A[0][0])
+print(matrix_A[0][2])
+print(matrix_A[2][0])
+# matrix_A[outer][inner]
 
 # R C 
 matrix_a = [[1, 2],  
