ff183e4a0be57c27c2ed265488db42ef54dde41a
BobAfwata <bobraphtoneafwata@gmail.com>
2025-02-27 22:54:24 +0900

    file io

---
+++ file_operations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import os
+
+os.rmdir("C:/Users/USER/Documents/Mechatronics/folder")
+"""
+
+# open the file in reading mode 
+f = open("C:/Users/USER/Documents/Mechatronics/data.txt" ,"w")
+# read whole file
+#print(f.read())
+
+f.write("Its fun learning file operations")
+
+#print(f.readline())
+f.close()
+#print(f.readline())
+
+"""
